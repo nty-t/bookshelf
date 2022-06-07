c957e24acbe2fbfc0f4e4da166aecb8a8d4ec017
nty <nathan11002@gmail.com>
2022-06-07 13:55:27 +0300

    fixed the bug where users are logged out when page is reloadded by adding a token

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -7,11 +7,29 @@ import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
 
+import {client} from './utils/api-client'
+
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
+
+
 function App() {
   // 🐨 useState for the user
   const [user, setUser] = React.useState(null)
 
   // 🐨 create a login function that calls auth.login then sets the user
+  React.useEffect(() => {
+    getUser().then(u => setUser(u))
+  }, [])
 
   const login = form => auth.login(form).then(u => setUser(u))
   const register = form => auth.register(form).then(u => setUser(u))

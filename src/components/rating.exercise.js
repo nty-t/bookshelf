ef9a93dac60f7ba7b64267c5179955d21cb30bbb
nty <nathan11002@gmail.com>
2022-06-08 10:31:32 +0300

    added an update mutation on the rating component to update changes made

--- src/components/rating.exercise.js
+++ src/components/rating.exercise.js
@@ -4,6 +4,8 @@ import {jsx} from '@emotion/core'
 import * as React from 'react'
 // 🐨 you'll need useMutation and queryCache from react-query
 // 🐨 you'll also need the client from utils/api-client
+import {useMutation, queryCache} from 'react-query'
+import { client } from 'utils/api-client'
 import {FaStar} from 'react-icons/fa'
 import * as colors from 'styles/colors'
 
@@ -25,8 +27,16 @@ function Rating({listItem, user}) {
   //   and the updates as data. The mutate function will be called with the updates
   //   you can pass as data.
   // 💰 if you want to get the list-items cache updated after this query finishes
-  // the use the `onSettled` config option to queryCache.invalidateQueries('list-items')
-  const update = () => {}
+  // the use the `onSettled` config option to queryCache.invalidateQueries('list-items') 
+  const [update] = useMutation(
+    updates =>
+      client(`list-items/${updates.id}`, {
+        method: 'PUT',
+        data: updates,
+        token: user.token,
+      }),
+    {onSettled: () => queryCache.invalidateQueries('list-items')},
+  )
 
   React.useEffect(() => {
     function handleKeyDown(event) {

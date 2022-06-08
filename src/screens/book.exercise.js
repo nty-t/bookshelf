c253d80f3fa361c24fe72058ed8c6ec2653ed473
nty <nathan11002@gmail.com>
2022-06-08 15:07:56 +0300

    made a useUpdateListItem hook to handle useMutation

--- src/screens/book.exercise.js
+++ src/screens/book.exercise.js
@@ -6,48 +6,19 @@ import debounceFn from 'debounce-fn'
 import {FaRegCalendarAlt} from 'react-icons/fa'
 import Tooltip from '@reach/tooltip'
 import {useParams} from 'react-router-dom'
-// 🐨 you'll need these:
-import {useQuery, useMutation, queryCache} from 'react-query'
-// import {useAsync} from 'utils/hooks'
-import {client} from 'utils/api-client'
 import {formatDate} from 'utils/misc'
 import * as mq from 'styles/media-queries'
 import * as colors from 'styles/colors'
 import {Textarea} from 'components/lib'
 import {Rating} from 'components/rating'
 import {StatusButtons} from 'components/status-buttons'
-import { useBook} from 'utils/books'
+import {useBook} from 'utils/books'
+import {useListItem, useUpdateListItem} from 'utils/list-items'
 
 function BookScreen({user}) {
   const {bookId} = useParams()
-  // 💣 remove the useAsync call here
-  // const {data, run} = useAsync()
-
-  // 🐨 call useQuery here
-  // queryKey should be ['book', {bookId}]
-  // queryFn should be what's currently passed in the run function below
   const book = useBook(bookId, user)
-
-  // 💣 remove the useEffect here (react-query will handle that now)
-  // React.useEffect(() => {
-  //   run(client(`books/${bookId}`, {token: user.token}))
-  // }, [run, bookId, user.token])
-
-  // 🐨 call useQuery to get the list item from the list-items endpoint
-  // queryKey should be 'list-items'
-  // queryFn should call the 'list-items' endpoint with the user's token
-  const {data: listItems} = useQuery({
-    queryKey: 'list-items',
-    queryFn: () =>
-      client('list-items', {token: user.token}).then(data => data.listItems),
-  })
-  const listItem = listItems?.find(li => li.bookId === book.id) ?? null
-  // const listItem = null
-  // 🦉 NOTE: the backend doesn't support getting a single list-item by it's ID
-  // and instead expects us to cache all the list items and look them up in our
-  // cache. This works out because we're using react-query for caching!
-
-  // const book = data?.book ?? loadingBook
+  const listItem = useListItem(user, bookId)
   const {title, author, coverImageUrl, publisher, synopsis} = book
 
   return (
@@ -130,24 +101,9 @@ function ListItemTimeframe({listItem}) {
 }
 
 function NotesTextarea({listItem, user}) {
-  // 🐨 call useMutation here
-  // the mutate function should call the list-items/:listItemId endpoint with a PUT
-  //   and the updates as data. The mutate function will be called with the updates
-  //   you can pass as data.
-  // 💰 if you want to get the list-items cache updated after this query finishes
-  // the use the `onSettled` config option to queryCache.invalidateQueries('list-items')
-  // 💣 DELETE THIS ESLINT IGNORE!! Don't ignore the exhaustive deps rule please
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  // const mutate = () => {}
-  const [mutate] = useMutation(
-    updates =>
-      client(`list-items/${updates.id}`, {
-        method: 'PUT',
-        data: updates,
-        token: user.token,
-      }),
-    {onSettled: () => queryCache.invalidateQueries('list-items')},
-  )
+  const [mutate] = useUpdateListItem(user)
+
+
   const debouncedMutate = React.useMemo(
     () => debounceFn(mutate, {wait: 300}),
     [mutate],
--- src/utils/list-items.exercise.js
+++ src/utils/list-items.exercise.js
@@ -1,4 +1,4 @@
-import {useQuery} from 'react-query'
+import {useQuery, useMutation, queryCache} from 'react-query'
 import {client} from 'utils/api-client'
 
 function useListItems(user) {
@@ -8,11 +8,23 @@ function useListItems(user) {
       client('list-items', {token: user.token}).then(data => data.listItems),
   })
   return listItems ?? []
- }
-
-function useListItem(user, bookId) {
-    const listItems = useListItems(user)
-    return  listItems?.find(li => li.bookId === bookId) ?? null
 }
 
-export {useListItem, useListItems}
+function useListItem(user, bookId) {
+  const listItems = useListItems(user)
+  return listItems?.find(li => li.bookId === bookId) ?? null
+}
+
+function useUpdateListItem(user) {
+  return useMutation(
+    updates =>
+      client(`list-items/${updates.id}`, {
+        method: 'PUT',
+        data: updates,
+        token: user.token,
+      }),
+    {onSettled: () => queryCache.invalidateQueries('list-items')},
+  )
+}
+
+export {useListItem, useListItems, useUpdateListItem}

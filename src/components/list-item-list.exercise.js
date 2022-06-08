f015caac74be9db58f6446b67c008a0e4e812f8b
nty <nathan11002@gmail.com>
2022-06-08 11:49:41 +0300

    added listitems usequery to handle the finished books list

--- src/components/list-item-list.exercise.js
+++ src/components/list-item-list.exercise.js
@@ -3,6 +3,8 @@ import {jsx} from '@emotion/core'
 
 // 🐨 you'll need useQuery from 'react-query'
 // 🐨 and client from 'utils/api-client'
+import { useQuery } from 'react-query'
+import {client} from 'utils/api-client'
 import {BookListUL} from './lib'
 import {BookRow} from './book-row'
 
@@ -17,7 +19,11 @@ function ListItemList({
   // queryFn should call the 'list-items' endpoint
 
   // 🐨 assign this to the list items you get back from react-query
-  const listItems = null
+  const {data: listItems} = useQuery({
+    queryKey: 'list-items',
+    queryFn: () =>
+      client('list-items', {token: user.token}).then(data => data.listItems),
+  })
 
   const filteredListItems = listItems?.filter(filterListItems)
 
